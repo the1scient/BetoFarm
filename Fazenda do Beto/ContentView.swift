d84a2750f64ccf3036903b48b1428f0b7b665f5c
GUILHERME FAGGION FABBRI <guilherme.ffabbri@CAS681403I441A.local>
2024-04-19 14:52:35 -0300

    test

---
+++ Fazenda do Beto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fazenda do Beto
+//
+//  Created by GUILHERME FAGGION FABBRI on 18/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("a")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fazenda do Beto/Fazenda_do_BetoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fazenda_do_BetoApp.swift
+//  Fazenda do Beto
+//
+//  Created by GUILHERME FAGGION FABBRI on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Fazenda_do_BetoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
